d4ced2149cf80a7f2402e9168d84d7da1ea4f3f4
RahulShukla <rahultest@gmail.com>
2022-01-07 21:53:15 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,15 @@
+l = [4, 7, 8, 3, 2, 1]
+l.sort()
+print(f'the sorted array is {l}')
+
+
+name = "rahul"
+name.upper()
+print(f'the upper case name is {name.upper()}')
+
+
+integer_list = []
+string_list = ["1", "2", "3", "4", "5"]
+for i in string_list:
+    integer_list.append(int(i))
+print("converting string into the integer", integer_list)
